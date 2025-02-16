f8a8220697e9f8db378fdd0b85adb7d3981f7889
SimonDiTomasso <simon.ditomasso@mail.mcgill.ca>
2025-02-16 13:44:32 -0500

    fixed gradle setup BACKEND

---
+++ NomNomApp_Backend/src/main/java/com/example/NomNomApp/NomNomAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.NomNomApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomNomAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NomNomAppApplication.class, args);
+	}
+
+}
