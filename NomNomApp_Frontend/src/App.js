5446794d91aae4dfbe45ee80b1edbf3236e787db
SimonDiTomasso <simon.ditomasso@mail.mcgill.ca>
2025-02-18 18:27:34 -0500

    Added Frontend
    The frontend is added next to the backend file. We are using React

---
+++ NomNomApp_Frontend/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          YAY FRONTEND IS SETUP
+        </p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        hey group 1 click this for react help
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
