f2b7e799ef5c5014816f5c914edb278594f0f578
SimonDiTomasso <simon.ditomasso@mail.mcgill.ca>
2025-02-23 15:35:29 -0500

    Automated tests for RecipeService done

--- NomNomApp_Backend/src/main/java/com/example/nomnomapp/service/RecipeService.java
+++ NomNomApp_Backend/src/main/java/com/example/nomnomapp/service/RecipeService.java
@@ -1,6 +1,7 @@
 package com.example.nomnomapp.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,6 +21,20 @@ public class RecipeService {
 
     }
 
+    /**
+     * Deletes a recipe by id.
+     *
+     * @param recipeId id of the recipe to delete.
+     * @throws IllegalArgumentException if the recipe does not exist.
+    */
+    public void deleteRecipeById(Integer recipeId) {
+        Optional<Recipe> recipe = recipeRepository.findById(recipeId);
+        if (recipe.isPresent()) {
+            recipeRepository.delete(recipe.get());
+        } else {
+            throw new IllegalArgumentException("Recipe with ID '" + recipeId + "' not found.");
+        }
+    }
 
     public Recipe getRecipeByID(int recipeID) {
         return recipeRepository.findRecipeById(recipeID);
