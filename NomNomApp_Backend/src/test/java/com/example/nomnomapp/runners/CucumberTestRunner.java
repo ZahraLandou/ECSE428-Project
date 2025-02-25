41683cc5e5b5161d012e5212e6ffc097005dea11
Kamabra Kazuto <97198979+SzmiSmi@users.noreply.github.com>
2025-02-26 03:32:25 +0900

    upped jupiter version, swapped to org.junit.platform.suite.api.*;

--- NomNomApp_Backend/src/test/java/com/example/nomnomapp/runners/CucumberTestRunner.java
+++ NomNomApp_Backend/src/test/java/com/example/nomnomapp/runners/CucumberTestRunner.java
@@ -1,9 +1,10 @@
 package com.example.nomnomapp.runners;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+// import org.junit.platform.suite.api.ConfigurationParameter;
+// import org.junit.platform.suite.api.IncludeEngines;
+// import org.junit.platform.suite.api.SelectClasspathResource;
+// import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.*;
 import io.cucumber.junit.platform.engine.Constants;
 
 @Suite
