38c8610e202704140df38f2c3328d62724df854c
ym-liu <ymliuy@gmail.com>
2025-03-20 16:42:40 -0400

    test: add @given step definitions for viewing recipe lists

--- NomNomApp_Backend/src/test/java/com/example/nomnomapp/stepdefinitions/ViewCollectionsStepDefinitions.java
+++ NomNomApp_Backend/src/test/java/com/example/nomnomapp/stepdefinitions/ViewCollectionsStepDefinitions.java
@@ -1,20 +1,24 @@
 package com.example.nomnomapp.stepdefinitions;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 import com.example.nomnomapp.model.RecipeList;
+import com.example.nomnomapp.model.Recipe.RecipeCategory;
 import com.example.nomnomapp.model.Recipe;
 import com.example.nomnomapp.model.NomNomUser;
-
+import com.example.nomnomapp.model.Rating;
 import com.example.nomnomapp.service.RecipeListService;
 import com.example.nomnomapp.service.RecipeService;
 import com.example.nomnomapp.service.UserService;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.sql.Date;
 import java.util.*;
 
 public class ViewCollectionsStepDefinitions {
@@ -28,21 +32,100 @@ public class ViewCollectionsStepDefinitions {
     @Autowired
     private UserService userService;
 
+    private NomNomUser testUser;
     private final Map<String, Recipe> recipeDatabase = new HashMap<>(); // Simulated recipe storage
-    private final Map<String, RecipeList> collectionDatabase = new HashMap<>(); // Simulated recipe storage
+    private final Map<String, RecipeList> recipeListDatabase = new HashMap<>(); // Simulated recipe storage
     private Exception exception;
 
     @Before
     public void setUp() {
         recipeDatabase.clear();
-        collectionDatabase.clear();
+        recipeListDatabase.clear();
         exception = null;
     }
 
     @After
     public void cleanUp() {
         recipeDatabase.clear();
-        collectionDatabase.clear();
+        recipeListDatabase.clear();
     }
 
+    // ----------------------------------------------------------------------
+    // GIVEN
+    // ----------------------------------------------------------------------
+
+    @Given("the user is logged into the NomNom application")
+    public void the_user_is_logged_into_the_NomNom_application() {
+
+        testUser = new NomNomUser("testUser", "test@example.com", "password");
+        testUser.setUserId(1);
+    }
+
+    @Given("the user is not logged into the NomNom application")
+    public void the_user_is_not_logged_into_the_NomNom_application() {
+
+        testUser = null;
+    }
+
+    @Given("the user has created the {name} list, of category \"Regular\"")
+    public void the_user_has_created_the_list_of_category_regular(String name) {
+
+        RecipeList recipeList = new RecipeList();
+
+        recipeList.setName(name);
+        recipeList.setCategory(RecipeList.ListCategory.Regular);
+        recipeList.setNomNomUser(testUser);
+
+        recipeListDatabase.put(name, recipeList);
+    }
+
+    @Given("the user has saved the following recipes in the {name} list")
+    public void the_user_has_saved_the_following_recipes_in_the_list(String name, DataTable dataTable) {
+
+        RecipeList recipeList = recipeListDatabase.get(name);
+        if (recipeList == null) {
+            the_user_has_created_the_list_of_category_regular(name);
+        }
+
+        List<Map<String, String>> recipeData = dataTable.asMaps(String.class, String.class);
+
+        for (Map<String, String> recipeRow : recipeData) {
+            // get table data
+            String title = recipeRow.get("name");
+            String description = recipeRow.get("description");
+
+            // create new recipe
+            Recipe recipe = new Recipe();
+            recipe.setTitle(title);
+            recipe.setDescription(description);
+            recipe.setCreationDate(new Date(System.currentTimeMillis()));
+
+            // link recipe to user
+            recipe.setNomNomUser(testUser);
+            testUser.addRecipe(recipe);
+
+            // link recipe to list
+            recipeList.addRecipe(recipe);
+
+            // update database
+            recipeDatabase.put(title, recipe);
+            recipeListDatabase.put(name, recipeList);
+        }
+    }
+
+    @Given("the user has no recipes saved in their {name} list")
+    public void the_user_has_no_recipes_saved_in_their_list(String name) {
+
+        // remove old recipe list
+        RecipeList recipeList = recipeListDatabase.get(name);
+        if (recipeList != null) {
+            for (Recipe recipe : recipeList.getRecipes()) {
+                recipeDatabase.remove(recipe.getTitle());
+            }
+            recipeListDatabase.remove(name);
+        }
+
+        // create new recipe list with no recipes in it
+        the_user_has_created_the_list_of_category_regular(name);
+    }
 }
--- NomNomApp_Backend/src/test/resources/features/ViewCollections.feature
+++ NomNomApp_Backend/src/test/resources/features/ViewCollections.feature
@@ -6,7 +6,7 @@ Feature: View collection of recipes
     # normal flow
     Scenario: Successfully view collection of recipes (normal flow)
         Given the user is logged into the NomNom application
-        And the user has created the "<name>" list, of category "<category>"
+        And the user has created the "<name>" list, of category "Regular"
         And the user has saved the following recipes in the "<name>" list
             | name                    | description                          |
             | Victorian Hot Chocolate | A delicious old-timey cocoa brew     |
@@ -18,32 +18,26 @@ Feature: View collection of recipes
             | Medieval Fantasy Gruel  | A warm hearty stew to heal your soul |
 
         Examples:
-            | name                      | category |
-            | Historical Cuisine To Try | Regular  |
+            | name                      |
+            | Historical Cuisine To Try |
 
     # alternative flow
     Scenario: Successfully view empty collection of recipes (alternative flow)
         Given the user is logged into the NomNom application
-        And the user has created the "<name>" list, of category "<category>"
-        And the user has no recipes saved their "<name>" list
+        And the user has created the "<name>" list, of category "Regular"
+        And the user has no recipes saved in their "<name>" list
         When the user navigates to the "<name>" list
         Then the NomNom application should display "<message>"
 
         Examples:
-            | name                      | category | message                                |
-            | Historical Cuisine To Try | Regular  | You have no recipes in this collection |
+            | name                      | message                                |
+            | Historical Cuisine To Try | You have no recipes in this collection |
 
     # error flow
-    Scenario: Unsuccessfully view another user's privated collection of recipes (error flow)
-        Given the user is logged into the NomNom application
-        And a different user has created the "<name>" list, of category "<category>"
-        And the different user has privated the "<name>" list
-        And the different user has saved the following recipes in the "<name>" list
-            | name                    | description                          |
-            | Victorian Hot Chocolate | A delicious old-timey cocoa brew     |
-            | Medieval Fantasy Gruel  | A warm hearty stew to heal your soul |
-        When the user navigates to the other user's "<name>" list
+    Scenario: Unsuccessfully view collection of recipes while logged out (error flow)
+        Given the user is not logged into the NomNom application
+        When the user navigates to the "<name>" list
         Then the NomNom application should display "<message>"
         Examples:
-            | name                      | category | message                    |
-            | Historical Cuisine To Try | Regular  | This collection is private |
+            | name                      | message               |
+            | Historical Cuisine To Try | You are not logged in |
